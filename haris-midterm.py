a01a2d548f0a7e061f49e1fc065de0d39a393f0f
muhammad5haris <156824837+muhammad5haris@users.noreply.github.com>
2024-01-18 21:58:54 +0500

    Add files via upload

---
+++ haris-midterm.py
@@ -0,0 +1,36 @@
+# Muhammad Haris
+# mid term exam
+
+# Q1 - Write a Python program to do arithmetical operations addition and division.
+f=int(input("enter a first number "))
+g=int(input("enter a second number" ))
+addition=f+g
+print(addition)
+
+a=int(input("enter a first number"))
+b=int(input("enter a second number"))
+division=a/b
+print(division)
+
+
+# Q2 - Write a Python program to find the area of a triangle
+c=int(input("enter a base of triangle"))
+d=int(input("enter a height of triangle"))
+area=1/2*c*d
+print(area)
+
+
+# Q3 - Write a Python program to convert Celsius to Fahrenheit.
+celsius=int(input("enter a temperature in celsius"))
+fahrenheit=9/5*celsius+32
+print(fahrenheit)
+
+# Q4 - Write a Python program that return all datatypes that we discussed in the class.
+num=19
+float=13.5
+str="Haris"
+bool=True
+print("this variable is a type of:",type(num))
+print("this variable is a type of:",type(float))
+print("this variable is a type of:",type(str))
+print("this variable is a type of:",type(bool))
